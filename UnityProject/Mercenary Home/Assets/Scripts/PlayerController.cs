43ad4395cad0b9fdb8da2a69af26274452ae1533
FireHogGames <hidde01@live.nl>
2019-12-04 09:45:18 +0100

    Started making character controller

---
+++ UnityProject/Mercenary Home/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed;
+    private float gravity;
+
+    private Vector3 movement;
+
+    private float _xMov;
+    private float _zMov;
+    private float _xRot;
+    private float _yRot;
+}
